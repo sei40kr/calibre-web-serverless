a31b6e75978981a4252f0839846af04f63632d0a
Seong Yong-ju <sei40kr@gmail.com>
2026-01-20 01:11:33 +0900

    chore: add git-hooks.nix with biome, nil, statix, and treefmt

--- flake.nix
+++ flake.nix
@@ -8,6 +8,10 @@
       url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
@@ -16,6 +20,7 @@
       nixpkgs,
       flake-utils,
       treefmt-nix,
+      git-hooks,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
@@ -36,16 +41,33 @@
             };
           };
         };
+
+        pre-commit-check = git-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            biome.enable = true;
+            nil.enable = true;
+            statix.enable = true;
+            treefmt = {
+              enable = true;
+              package = treefmtEval.config.build.wrapper;
+            };
+          };
+        };
       in
       {
         formatter = treefmtEval.config.build.wrapper;
 
         checks = {
+          inherit pre-commit-check;
           formatting = treefmtEval.config.build.check self;
         };
 
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [ bun ];
+          shellHook = ''
+            ${pre-commit-check.shellHook}
+          '';
         };
       }
     );

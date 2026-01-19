22ab3849dda1ec6a0fb61af25ad7288a50a1fcc2
Seong Yong-ju <sei40kr@gmail.com>
2026-01-20 00:55:28 +0900

    chore: add nix flake for development environment

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Calibre Web Serverless - Next.js application";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [ bun ];
+        };
+      }
+    );
+}

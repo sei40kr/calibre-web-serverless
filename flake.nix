31bdfb0a610008979a97288e4f4cd67b0367aefa
Seong Yong-ju <sei40kr@gmail.com>
2026-01-20 01:01:12 +0900

    chore: add treefmt-nix configuration for code formatting

--- flake.nix
+++ flake.nix
@@ -4,6 +4,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
@@ -11,6 +15,7 @@
       self,
       nixpkgs,
       flake-utils,
+      treefmt-nix,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
@@ -18,8 +23,27 @@
         pkgs = import nixpkgs {
           inherit system;
         };
+
+        treefmtEval = treefmt-nix.lib.evalModule pkgs {
+          projectRootFile = "flake.nix";
+          programs = {
+            biome.enable = true;
+            nixfmt.enable = true;
+            statix.enable = true;
+            prettier = {
+              enable = true;
+              includes = [ "*.md" ];
+            };
+          };
+        };
       in
       {
+        formatter = treefmtEval.config.build.wrapper;
+
+        checks = {
+          formatting = treefmtEval.config.build.check self;
+        };
+
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [ bun ];
         };
